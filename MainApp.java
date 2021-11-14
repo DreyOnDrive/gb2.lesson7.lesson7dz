e48f160ef6048879293ce3e086ef6fd305ad5da9
DreyOnDrive <85063697+DreyOnDrive@users.noreply.github.com>
2021-11-14 12:27:48 +0300

    Update MainApp.java

--- MainApp.java
+++ MainApp.java
@@ -1,1 +1,11 @@
+package gb2.lesson7.lesson7dz;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+
+public class MainApp {
+
+    public static void main(String[] args) throws JsonProcessingException {
+        UserInterface userInterface = new UserInterface();
+        userInterface.runApplication();
+    }
+}
